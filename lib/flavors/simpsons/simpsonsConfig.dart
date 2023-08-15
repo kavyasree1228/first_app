77eb35fd4ae0465cb9d2bc8d981f18982d912827
kavyasree1228 <53187741+kavyasree1228@users.noreply.github.com>
2023-08-14 21:51:00 -0400

    Flavour config files .

---
+++ lib/flavors/simpsons/simpsonsConfig.dart
@@ -0,0 +1,5 @@
+class AppConfig {
+  static const String apiUrl =
+      'http://api.duckduckgo.com/?q=simpsons+characters&format=json';
+  static const String packageName = 'com.sample.simpsonsviewer';
+}
---
+++ lib/flavors/the_wire/wireConfig.dart
@@ -0,0 +1,5 @@
+class AppConfig {
+  static const String apiUrl =
+      'http://api.duckduckgo.com/?q=the+wire+characters&format=json';
+  static const String packageName = 'com.sample.wireviewer';
+}
